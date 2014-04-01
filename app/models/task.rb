8f723caca05093b97a56d02acc8bb9d471aabfd9
Ali Abou-Zahr & Nathan Grey <student@epicodus.com>
2014-03-31 17:36:49 -0700

    dsiplays done and not done items

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,11 @@
 class Task < ActiveRecord::Base
   validates :description, :presence => true
+
+  def self.done
+    Task.where({done: true})
+  end
+
+  def self.not_done
+    Task.where({done: false})
+  end
 end
--- spec/models/tasks_spec.rb
+++ spec/models/tasks_spec.rb
@@ -2,4 +2,12 @@ require 'spec_helper'
 
 describe Task do
   it {should validate_presence_of :description}
+
+  describe '.done' do
+    it 'should show all the tasks that are complete' do
+      task = Task.create({description: 'clean'})
+      task.update(done: true)
+      Task.done.should eq [task]
+    end
+  end
 end
